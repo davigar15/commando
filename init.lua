1d7446035a3c3490d509780c79764f43a1f4b169
David Garcia <davigar15@gmail.com>
2025-05-22 06:47:51 +0200

    Apply formatting

--- init.lua
+++ init.lua
@@ -10,29 +10,29 @@ local Commando = {}
 --- Run a command in tmux via Vimux
 ---@param cmd string
 local function run_in_tmux(cmd)
-	vim.cmd("VimuxOpenRunner")
-	vim.cmd("VimuxRunCommand('clear; echo -e \"" .. cmd .. '"; ' .. cmd .. "')")
+    vim.cmd("VimuxOpenRunner")
+    vim.cmd("VimuxRunCommand('clear; echo -e \"" .. cmd .. '"; ' .. cmd .. "')")
 end
 
 --- Replace custom placeholders in a command string
 ---@param cmd string
 ---@return string
 local function replace_placeholders(cmd)
-	cmd = cmd:gsub("{test_file}", CommandoUtils.get_test_file())
-	cmd = cmd:gsub("{test_nearest}", CommandoUtils.get_nearest_test())
-	return cmd
+    cmd = cmd:gsub("{test_file}", CommandoUtils.get_test_file())
+    cmd = cmd:gsub("{test_nearest}", CommandoUtils.get_nearest_test())
+    return cmd
 end
 
 -- Register custom Harpoon configuration for "commando"
 harpoon:setup({
-	commando = {
-		---@param list_item HarpoonListItem
-		---@param list unknown
-		---@param options unknown
-		select = function(list_item, list, options)
-			Commando.run(list_item.context.row)
-		end,
-	},
+    commando = {
+        ---@param list_item HarpoonListItem
+        ---@param list unknown
+        ---@param options unknown
+        select = function(list_item, list, options)
+            Commando.run(list_item.context.row)
+        end,
+    },
 })
 
 --- @type HarpoonList
@@ -40,23 +40,23 @@ Commando.commando_list = harpoon:list("commando")
 
 --- Show Harpoon UI for Commando list
 function Commando.show()
-	harpoon.ui:toggle_quick_menu(Commando.commando_list, {
-		title = "Commando",
-		title_pos = "center",
-		border = "double",
-	})
+    harpoon.ui:toggle_quick_menu(Commando.commando_list, {
+        title = "Commando",
+        title_pos = "center",
+        border = "double",
+    })
 end
 
 --- Run command from Harpoon list by index
 ---@param index integer
 function Commando.run(index)
-	local item = Commando.commando_list:get(index)
-	if item and item.value then
-		local cmd = replace_placeholders(item.value)
-		run_in_tmux(cmd)
-	else
-		vim.notify("No command found at index " .. index, vim.log.levels.WARN)
-	end
+    local item = Commando.commando_list:get(index)
+    if item and item.value then
+        local cmd = replace_placeholders(item.value)
+        run_in_tmux(cmd)
+    else
+        vim.notify("No command found at index " .. index, vim.log.levels.WARN)
+    end
 end
 
 return Commando
--- utils.lua
+++ utils.lua
@@ -3,48 +3,46 @@ local CommandoUtils = {}
 
 --- Get the current file path
 ---@return string
-function CommandoUtils.get_test_file()
-	return vim.fn.expand("%")
-end
+function CommandoUtils.get_test_file() return vim.fn.expand("%") end
 
 --- Get the nearest test function or class-based test above the current line
 --- Format: "filename::ClassName::test_name" or "filename::test_name"
 ---@return string
 function CommandoUtils.get_nearest_test()
-	local file = vim.fn.expand("%")
-	local current_line = vim.fn.line(".")
-	local nearest_test ---@type string|nil
-	local nearest_class ---@type string|nil
+    local file = vim.fn.expand("%")
+    local current_line = vim.fn.line(".")
+    local nearest_test ---@type string|nil
+    local nearest_class ---@type string|nil
 
-	-- Search upwards for the nearest test function
-	for i = current_line, 1, -1 do
-		local line = vim.fn.getline(i)
-		local test_name = line:match("^%s*def%s+(test[%w_]+)%s*%(")
-		if test_name then
-			nearest_test = test_name
-			break
-		end
-	end
+    -- Search upwards for the nearest test function
+    for i = current_line, 1, -1 do
+        local line = vim.fn.getline(i)
+        local test_name = line:match("^%s*def%s+(test[%w_]+)%s*%(")
+        if test_name then
+            nearest_test = test_name
+            break
+        end
+    end
 
-	if not nearest_test then
-		return file
-	end
+    if not nearest_test then
+        return file
+    end
 
-	-- Now search upwards for the class definition
-	for i = current_line, 1, -1 do
-		local line = vim.fn.getline(i)
-		local class_name = line:match("^%s*class%s+([%w_]+)")
-		if class_name then
-			nearest_class = class_name
-			break
-		end
-	end
+    -- Now search upwards for the class definition
+    for i = current_line, 1, -1 do
+        local line = vim.fn.getline(i)
+        local class_name = line:match("^%s*class%s+([%w_]+)")
+        if class_name then
+            nearest_class = class_name
+            break
+        end
+    end
 
-	if nearest_class then
-		return file .. "::" .. nearest_class .. "::" .. nearest_test
-	else
-		return file .. "::" .. nearest_test
-	end
+    if nearest_class then
+        return file .. "::" .. nearest_class .. "::" .. nearest_test
+    else
+        return file .. "::" .. nearest_test
+    end
 end
 
 return CommandoUtils
